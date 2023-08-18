e6b26ed77f5ba26d0d256422f2fb37473852cd9d
Tushar Phondge <phondge@gmail.com>
2023-08-18 18:09:39 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,22 @@ import streamlit as st
 from hugchat import hugchat
 from hugchat.login import Login
 
-st.set_page_config(page_title="🤗💬 LetsChat")
+st.set_page_config(page_title="🤗💬 HugChat")
 
 st.write('Hello world!')
+
+with st.sidebar:
+    st.title('🤗💬 LetsChat')
+    if ('EMAIL' in st.secrets) and ('PASS' in st.secrets):
+        st.success('HuggingFace Login credentials already provided!', icon='✅')
+        hf_email = st.secrets['EMAIL']
+        hf_pass = st.secrets['PASS']
+    else:
+        hf_email = st.text_input('Enter E-mail:', type='password')
+        hf_pass = st.text_input('Enter password:', type='password')
+        if not (hf_email and hf_pass):
+            st.warning('Please enter your credentials!', icon='⚠️')
+        else:
+            st.success('Proceed to entering your prompt message!', icon='👉')
+    st.markdown('📖 Learn how to build this app in this [blog](https://blog.streamlit.io/how-to-build-an-llm-powered-chatbot-with-streamlit/)!')
+Use the with statement to confine the constituent contents to the sidebar. They
